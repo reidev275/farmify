619183cd10eb83403178012fe087f111aac13418
Reid Evans <reidev275@gmail.com>
2019-07-30 16:02:16 -0400

    move to property based testing

--- test/validation.ts
+++ test/validation.ts
@@ -1,6 +1,7 @@
 import * as V from "../src/validation";
 import { expect } from "chai";
 import "mocha";
+import * as fc from "fast-check";
 
 type Person = { first: string; age: number };
 
@@ -12,20 +13,30 @@ const firstIsDefined = (p: Person): V.Validation<string> =>
 
 const validate = (p: Person) => V.combine(ageIsPositive(p), firstIsDefined(p));
 
-const successfulPerson = { first: "Reid", age: 1 };
 const failurePerson = { first: undefined, age: -1 };
 
 describe("Validation Combination", () => {
   it("is Successful when it should be", () => {
-    const result = validate(successfulPerson);
-    expect(result.kind).to.equal("Success");
+    fc.assert(
+      fc.property(
+        fc.string(1, 10),
+        fc.integer(0, 99999),
+        (first, age) => validate({ first, age }).kind == "Success"
+      )
+    );
   });
 
   it("is Failure when it should be", () => {
-    const result = validate(failurePerson);
-    expect(result.kind).to.equal("Failure");
-
-    const failures = V.cata(result, () => 0, e => e.length);
-    expect(failures).to.equal(2);
+    fc.assert(
+      fc.property(
+        fc.integer(0),
+        age =>
+          V.cata(
+            validate({ first: undefined, age: age }),
+            () => 0,
+            e => e.length
+          ) == 2
+      )
+    );
   });
 });
